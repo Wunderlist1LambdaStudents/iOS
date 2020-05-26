f5ad316dfb961b877bb6a713324829813cf7513b
Brad <braddiroff@icloud.com>
2020-05-26 18:37:02 -0400

    Putting storyboard together for use

---
+++ Wunderlist/Wunderlist/ViewControllers/SignUpInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpInViewController.swift
+//  Wunderlist
+//
+//  Created by Bradley Diroff on 5/26/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class SignUpInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
