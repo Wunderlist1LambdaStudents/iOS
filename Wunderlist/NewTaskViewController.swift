cd0e15a5e34ba8b5910959fc6b9670e247e23d1e
Akmal Nurmatov <akmalnurmatov1@gmail.com>
2020-05-27 14:33:26 -0400

    organized files, added NewTaskViewController and connected

---
+++ Wunderlist/NewTaskViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NewTaskViewController.swift
+//  Wunderlist
+//
+//  Created by Akmal Nurmatov on 5/27/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var notesTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveTaskButton(_ sender: UIButton) {
+    }
+    
+    
+}
