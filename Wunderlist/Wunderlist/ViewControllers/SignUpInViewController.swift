6cfafe94dfc2a507ff6b6d2a3ddff3e50da8e2f5
Vincent Hoang <vietnhoang7@gmail.com>
2020-05-28 21:20:29 -0700

    Fix login implementation

--- Wunderlist/Wunderlist/ViewControllers/SignUpInViewController.swift
+++ Wunderlist/Wunderlist/ViewControllers/SignUpInViewController.swift
@@ -15,6 +15,8 @@ class SignUpInViewController: UIViewController {
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var changeButton: UIButton!
     
+    private var isSignUpMode: Bool = false
+    
     let segueName = "loginSegue"
     
     override func viewDidLoad() {
@@ -27,27 +29,79 @@ class SignUpInViewController: UIViewController {
         let username = nameField.text ?? ""
         let password = passField.text ?? ""
         
+        
         if !username.isEmpty && !password.isEmpty {
-            
-            UserController.shared.loginUser(username: username, password: password) { result in
-                
-                do {
-                    let success = try result.get()
-                    if success {
-                        NSLog("Login successful")
-                    }
-                } catch {
-                    NSLog("Error logging in")
-                    return
-                }
+            if isSignUpMode {
+                UserController.shared.registerUser(username: username, password: password) { result in
+                    do {
+                        let success = try result.get()
+                        if success {
+                            NSLog("Sign up successful")
+                            
+                            DispatchQueue.main.async {
+                                let alert = UIAlertController(title: "Success", message: "You may now log in", preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: nil))
+                            
+                                self.present(alert, animated: true)
+                                self.toggleSignUpType()
+                            }
+                            
+                        } else {
+                            NSLog("Sign up unsuccessful")
+                            
+                            DispatchQueue.main.async {
+                                let alert = UIAlertController(title: "Error", message: "Unable to sign up", preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                                
+                                self.present(alert, animated: true)
+                            }
 
+                        }
+                    } catch {
+                        NSLog("Error signing up")
+                        return
+                    }
+                }
+            } else {
+                UserController.shared.loginUser(username: username, password: password) { result in
+                    do {
+                        let success = try result.get()
+                        if success {
+                            NSLog("Login successful")
+                            
+                            DispatchQueue.main.async {
+                                self.performSegue(withIdentifier: self.segueName, sender: nil)
+                            }
+                        }
+                    } catch {
+                        NSLog("Error logging in")
+                        return
+                    }
+                }
             }
-            performSegue(withIdentifier: segueName, sender: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Username and/or password fields must not be empty!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            
+            present(alert, animated: true)
         }
         
     }
     
     @IBAction func changeSignType(_ sender: Any) {
+        toggleSignUpType()
+    }
+    
+    private func toggleSignUpType() {
+        if isSignUpMode {
+            signInButton.setTitle("Sign In", for: .normal)
+            changeButton.setTitle("Don't have an account?", for: .normal)
+        } else {
+            signInButton.setTitle("Sign Up", for: .normal)
+            changeButton.setTitle("Already have an account?", for: .normal)
+        }
+        
+        isSignUpMode = !isSignUpMode
     }
     
     
