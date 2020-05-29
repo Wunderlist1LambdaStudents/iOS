315608988248607667a096d885e5a4015fb897af
Vincent Hoang <vietnhoang7@gmail.com>
2020-05-29 11:06:31 -0700

    Adjust cellForRowAt

--- Wunderlist/Wunderlist/ViewControllers/HomeViewController.swift
+++ Wunderlist/Wunderlist/ViewControllers/HomeViewController.swift
@@ -79,6 +79,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
                                                        for: indexPath) as? HomeTableViewCell else {return warningCell}
         
         cell.delegate = self
+        cell.entry = fetchedResultsController.object(at: indexPath)
         
         return cell
     }
